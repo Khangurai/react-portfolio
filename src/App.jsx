be3e02b88010f832d4ae6632808ae879e408a81c
Khangurai <79690068+Khangurai@users.noreply.github.com>
2025-08-05 23:18:59 +0630

    Updated package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { ConfigProvider } from "antd";
+import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import "./App.css";
-
-const theme = {
-  token: {
-    colorPrimary: '#1890ff',
-    borderRadius: 8,
-    fontFamily: 'Poppins, sans-serif',
-  },
-};
 
 const App = () => {
   return (
-    <ConfigProvider theme={theme}>
+    <>
       <Header />
+
       <main className="main">
         <Home />
       </main>
-    </ConfigProvider>
+    </>
   );
 };
 
