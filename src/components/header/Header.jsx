ecaede0802d63fd8cd60fb40a35a578c501f0652
Khangurai <79690068+Khangurai@users.noreply.github.com>
2025-08-05 22:16:40 +0630

    feat: implement Home component with Data and Social sections, add header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import "./header.css";
 
-
 const Header = () => {
+  /*=============== Toggle Menu ===============*/
+  const [Toggle, showMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">
           Aung
         </a>
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="uil uil-estate nav__icon"></i> 
+              <a href="#home" className="nav__link active">
+                <i className="uil uil-estate nav__icon"></i>
                 Home
               </a>
             </li>
@@ -42,6 +44,13 @@ const Header = () => {
               </a>
             </li>
           </ul>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/"
+        className="home__social-icon"
+        traget="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://github.com/"
+        className="home__social-icon"
+        traget="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+
+      <a
+        href="https://facebook.com/"
+        className="home__social-icon"
+        traget="_blank"
+      >
+        <i class="uil uil-facebook-f"></i>
+      </a>
+    </div>
+  );
+};
